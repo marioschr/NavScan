550187b63ef424560ca2f3cec68dbb0d81503547
Marios Chr <marios-christou@hotmail.com>
2021-08-11 20:02:34 +0300

    Designed RegisterFragment, created DatePicker for BirthDay

--- app/src/main/java/com/unipi/marioschr/navscan/RegisterFragment.java
+++ app/src/main/java/com/unipi/marioschr/navscan/RegisterFragment.java
@@ -1,63 +1,101 @@
 package com.unipi.marioschr.navscan;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link RegisterFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class RegisterFragment extends Fragment {
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Calendar;
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public RegisterFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RegisterFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static RegisterFragment newInstance(String param1, String param2) {
-        RegisterFragment fragment = new RegisterFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+public class RegisterFragment extends Fragment implements View.OnClickListener {
+    TextInputEditText tietBirthday;
+    TextInputLayout tilBirthday;
+    Button btnSignUp;
+    DatePickerDialog datePickerDialog;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_register, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        tietBirthday = view.findViewById(R.id.tietBirthday);
+        tilBirthday = view.findViewById(R.id.tilBirthday);
+        btnSignUp = view.findViewById(R.id.btnSignUp);
+
+        btnSignUp.setOnClickListener(this);
+        tietBirthday.setOnFocusChangeListener((v, hasFocus) -> {
+            if (hasFocus) {
+                openDatePicker();
+            } else {
+                hideDatePicker();
+            }
+        });
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btnSignUp:
+                signUp();
+                break;
+        }
+    }
+
+    private void signUp() {
+        Toast.makeText(getContext(), String.valueOf(tietBirthday.getText()), Toast.LENGTH_SHORT).show();
+    }
+
+    private void openDatePicker() {
+        // Get Current Date
+        final Calendar c = Calendar.getInstance();
+        int mYear = c.get(Calendar.YEAR);
+        int mMonth = c.get(Calendar.MONTH);
+        int mDay = c.get(Calendar.DAY_OF_MONTH);
+
+
+        if (String.valueOf(tietBirthday.getText()).equals("")) {
+            datePickerDialog = new DatePickerDialog(getContext(), (view, year, monthOfYear, dayOfMonth) -> {
+                tietBirthday.setText(String.format("%02d",dayOfMonth)
+                        + "-" + String.format("%02d",monthOfYear + 1)
+                        + "-" + String.format("%02d",year));
+                tietBirthday.clearFocus();
+            }, mYear, mMonth, mDay);
+        } else {
+            LocalDate localDate = LocalDate.parse(String.valueOf(tietBirthday.getText()), DateTimeFormatter.ofPattern("d-M-yyyy"));
+            datePickerDialog = new DatePickerDialog(getContext(), (view, year, monthOfYear, dayOfMonth) -> {
+                tietBirthday.setText(String.format("%02d",dayOfMonth)
+                        + "-" + String.format("%02d",monthOfYear + 1)
+                        + "-" + String.format("%02d",year));
+                tietBirthday.clearFocus();
+            }, localDate.getYear(), localDate.getMonthValue() - 1, localDate.getDayOfMonth());
+        }
+        datePickerDialog.getDatePicker().setMaxDate(c.getTimeInMillis());
+        Calendar minYear = Calendar.getInstance();
+        minYear.add(Calendar.YEAR, -110);
+        datePickerDialog.getDatePicker().setMinDate(minYear.getTimeInMillis());
+        datePickerDialog.setOnCancelListener(l -> tietBirthday.clearFocus());
+        datePickerDialog.show();
+    }
+
+    private void hideDatePicker() {
+        datePickerDialog.hide();
     }
 }
