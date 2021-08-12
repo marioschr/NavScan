20e86506361aa50170d8c205955dfd01d3a68fa2
Marios Chr <marios-christou@hotmail.com>
2021-08-12 14:39:47 +0300

    Register with Email Password, Pass user data into Firestore

--- app/src/main/java/com/unipi/marioschr/navscan/RegisterFragment.java
+++ app/src/main/java/com/unipi/marioschr/navscan/RegisterFragment.java
@@ -1,35 +1,55 @@
 package com.unipi.marioschr.navscan;
 
 import android.app.DatePickerDialog;
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+
+
+
 
 import com.google.android.material.textfield.TextInputEditText;
 import com.google.android.material.textfield.TextInputLayout;
+import com.google.firebase.Timestamp;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-import java.time.LocalDate;
+import java.time.LocalDate;;
+import java.time.ZoneOffset;
 import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class RegisterFragment extends Fragment implements View.OnClickListener {
-	TextInputEditText tietFirstName,tietLastName,tietBirthday,tietEmail,tietPassword,tietConfirmPassword;
-	TextInputLayout tilFirstName,tilLastName,tilBirthday,tilEmail,tilPassword,tilConfirmPassword;
-	Button btnSignUp;
-	DatePickerDialog datePickerDialog;
-	boolean foundError = false, passOk = true;
-
+	private TextInputEditText tietFirstName,tietLastName,tietBirthday,tietEmail,tietPassword,tietConfirmPassword;
+	private TextInputLayout tilFirstName,tilLastName,tilBirthday,tilEmail,tilPassword,tilConfirmPassword;
+	private TextView tvSignInRegisterFrag;
+	private Button btnSignUp;
+	private String firstName,lastName,birthday,email,password,confirmPassword;
+	private LocalDate birthdayLD;
+	private DatePickerDialog datePickerDialog;
+	private boolean foundError = false, passOk = true;
+	private FirebaseAuth mAuth;
+	private FirebaseFirestore db;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		return inflater.inflate(R.layout.fragment_register, container, false);
@@ -39,6 +59,8 @@ public class RegisterFragment extends Fragment implements View.OnClickListener {
 	public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
 		super.onViewCreated(view, savedInstanceState);
 		findViewsAndSetListeners(view);
+		mAuth = FirebaseAuth.getInstance();
+		db = FirebaseFirestore.getInstance();
 	}
 
 	private void findViewsAndSetListeners(View view) {
@@ -56,6 +78,8 @@ public class RegisterFragment extends Fragment implements View.OnClickListener {
 		tilEmail = view.findViewById(R.id.tilEmail);
 		tilPassword = view.findViewById(R.id.tilPassword);
 		tilConfirmPassword = view.findViewById(R.id.tilConfirmPassword);
+
+		tvSignInRegisterFrag = view.findViewById(R.id.tvSignInRegisterFrag);
 
 		btnSignUp = view.findViewById(R.id.btnSignUp);
 		//endregion
@@ -146,7 +170,7 @@ public class RegisterFragment extends Fragment implements View.OnClickListener {
 		//endregion
 
 		btnSignUp.setOnClickListener(this);
-
+		tvSignInRegisterFrag.setOnClickListener(this);
 		tietBirthday.setOnFocusChangeListener((v, hasFocus) -> {
 			if (hasFocus) openDatePicker();
 			else hideDatePicker();
@@ -159,21 +183,34 @@ public class RegisterFragment extends Fragment implements View.OnClickListener {
 			case R.id.btnSignUp:
 				signUp();
 				break;
+			case R.id.tvSignInRegisterFrag:
+				navigateBack();
+				break;
 		}
 	}
 
 	private void signUp() {
-		validateData();
+		if(!validateData()) return;
+		register(email,password);
 		//TODO:Create account with firebase
 	}
 
-	private void validateData() {
-		String firstName = String.valueOf(tietFirstName.getText());
-		String lastName = String.valueOf(tietLastName.getText());
-		String birthday = String.valueOf(tietBirthday.getText());
-		String email = String.valueOf(tietEmail.getText());
-		String password = String.valueOf(tietPassword.getText());
-		String confirmPassword = String.valueOf(tietConfirmPassword.getText());
+	private void navigateBack() {
+		NavHostFragment.findNavController(this).popBackStack();
+	}
+
+	private void navigateToMain() {
+		startActivity(new Intent(getActivity(), MainActivity.class));
+		requireActivity().finish();
+	}
+
+	private boolean validateData() {
+		firstName = String.valueOf(tietFirstName.getText());
+		lastName = String.valueOf(tietLastName.getText());
+		birthday = String.valueOf(tietBirthday.getText());
+		email = String.valueOf(tietEmail.getText());
+		password = String.valueOf(tietPassword.getText());
+		confirmPassword = String.valueOf(tietConfirmPassword.getText());
 
 		if (firstName.trim().isEmpty()) {
 			setErrorFirstName();
@@ -210,10 +247,48 @@ public class RegisterFragment extends Fragment implements View.OnClickListener {
 		passOk = true;
 		if (foundError) {
 			foundError = false;
-			return;
+			return false;
 		}
 		foundError = false;
 		Toast.makeText(getContext(), "OK", Toast.LENGTH_SHORT).show();
+		return true;
+	}
+
+	private void register(String email, String password) {
+		mAuth.createUserWithEmailAndPassword(email, password)
+				.addOnCompleteListener(requireActivity(), task -> {
+					if (task.isSuccessful()) {
+						// Sign in success, update UI with the signed-in user's information
+						Log.d("Firebase Register", "createUserWithEmail:success");
+						FirebaseUser user = mAuth.getCurrentUser();
+						Map<String, Object> userData = new HashMap<>();
+						userData.put("firstName", firstName);
+						userData.put("lastName", lastName);
+						userData.put("birthday", new Timestamp(new Date(birthdayLD.getMonth().toString() + " " + birthdayLD.getDayOfMonth() + ", " + birthdayLD.getYear())));
+						userData.put("email", email);
+
+						db.collection("users").document(user.getUid())
+								.set(userData)
+								.addOnSuccessListener(l -> {
+									Log.d("Firestore DB", "DocumentSnapshot successfully written!");
+									Toast.makeText(getContext(), "Successfully created user", Toast.LENGTH_SHORT).show();
+									navigateToMain();
+								})
+								.addOnFailureListener(e -> {
+									Log.w("Firestore DB", "Error writing document", e);
+									mAuth.getCurrentUser().delete().addOnSuccessListener(l -> {
+										Toast.makeText(getContext(), "Error creating user. Try again", Toast.LENGTH_SHORT).show();
+									});
+								});
+						//updateUI(user);
+					} else {
+						// If sign in fails, display a message to the user.
+						Log.w("Firebase Register", "createUserWithEmail:failure", task.getException());
+						Toast.makeText(getContext(), "Authentication failed.",
+								Toast.LENGTH_SHORT).show();
+						//updateUI(null);
+					}
+				});
 	}
 
 	//region Datepicker
@@ -231,6 +306,7 @@ public class RegisterFragment extends Fragment implements View.OnClickListener {
 				tietBirthday.setText(String.format("%02d", dayOfMonth)
 						+ "-" + String.format("%02d", monthOfYear + 1)
 						+ "-" + String.format("%02d", year));
+				birthdayLD = LocalDate.parse(String.valueOf(tietBirthday.getText()), DateTimeFormatter.ofPattern("dd-MM-yyyy"));
 				tietBirthday.clearFocus();
 			}, mYear, mMonth, mDay);
 		} else {
@@ -239,6 +315,7 @@ public class RegisterFragment extends Fragment implements View.OnClickListener {
 				tietBirthday.setText(String.format("%02d", dayOfMonth)
 						+ "-" + String.format("%02d", monthOfYear + 1)
 						+ "-" + String.format("%02d", year));
+				birthdayLD = LocalDate.parse(String.valueOf(tietBirthday.getText()), DateTimeFormatter.ofPattern("dd-MM-yyyy"));
 				tietBirthday.clearFocus();
 			}, localDate.getYear(), localDate.getMonthValue() - 1, localDate.getDayOfMonth());
 		}
